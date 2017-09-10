b4a1847ab5ef64b21a37e0a18310ea326609a7f2
auk <oyvind.auk@gmail.com>
2017-09-10 16:21:53 +0200

    Configured for Heroku (fourth try)

--- conserthub/settings/local.py
+++ conserthub/settings/local.py
@@ -85,6 +85,11 @@ DATABASES = {
         'PORT': '',
     }
 }
+#Heroku hobbu dev stuff
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
 
 
 # Password validation
