37fcf59a2d2e0a3d1a82e44ba353e9e5cfe519a2
auk <oyvind.auk@gmail.com>
2017-09-10 21:42:40 +0200

    statics heroku

--- conserthub/wsgi.py
+++ conserthub/wsgi.py
@@ -14,3 +14,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conserthub.settings")
 
 application = get_wsgi_application()
+
+
+
+#Heroku stuff
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
