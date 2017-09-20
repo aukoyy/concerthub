90cd4165ce0b314eb0cf1643ec3796626739d531
Aslak Hollund <aslakhol@gmail.com>
2017-09-20 23:37:49 +0200

    basic linting

--- booking/admin.py
+++ booking/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import (
     Stage,
     Concert,
 )
+
+# Register your models here.
+
 
 admin.site.register(Stage)
 
--- booking/views.py
+++ booking/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from .models import Concert
 
 # Create your views here.
 
-from .models import Concert
+
 def booking(request):
     template_name = "booking/firstpage.html"
 
