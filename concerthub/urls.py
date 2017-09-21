0e287ce9932d6b52c29fd453ad9b59fb58a925d5
Aslak Hollund <aslakhol@gmail.com>
2017-09-21 11:06:36 +0200

    updated urls for not yet existing loginpage

--- concerthub/urls.py
+++ concerthub/urls.py
@@ -21,7 +21,6 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 
-
 urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
