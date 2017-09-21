62ff7be46ddb23092d3b8ef1609c13d2fe6ff2fb
SondreWold <sondrewo@hotmail.com>
2017-09-21 10:28:54 +0200

    Fixed save function

--- booking/models.py
+++ booking/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 
-
-
 class Stage(models.Model):
     name = models.CharField(max_length=120)
     capacity = models.IntegerField()
     def __str__(self):
-
         return self.name
-
 
 class Concert(models.Model):
     artist = models.CharField(max_length=120)
@@ -20,13 +16,13 @@ class Concert(models.Model):
     tickets = models.IntegerField(null=True, blank=True)
     number_of_tech = models.IntegerField(null=True, blank=True)
 
-
     #Cheeky solution, switch to .clean() when enough knowledge.
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
         if self.stage.capacity < self.tickets:
             raise Exception("Cant add more tickets than stage capacity")
+        else:
+            super(Concert, self).save()
 
     def __str__(self):
         return self.artist
-
