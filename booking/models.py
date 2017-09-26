79a05723a7d04a4153a194b65852ca1f4d2a71eb
hannabg <hannabg@stud.ntnu.no>
2017-09-26 16:33:32 +0200

    quit Please enter the commit message for your changes. Lines starting

--- booking/models.py
+++ booking/models.py
@@ -1,20 +1,8 @@
 from django.db import models
 
 
-class Stage(models.Model):
-    name = models.CharField(max_length=120)
-    description = models.TextField(max_length=120)
-    audience_cap = models.IntegerField(null=True, blank=True)
-    festival_id = models.ForeignKey(Festival, null=True, blank=True)
-    #schedule?
-
-
-    def __str__(self):
-        return self.name
-
-
 class Festival(models.Model):
-    name = models.CharField(max_length=120)
+    name = models.CharField(max_length=120, null=True, blank=True)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
     start_time = models.TimeField(null=True, blank=True)
@@ -24,18 +12,29 @@ class Festival(models.Model):
     def _str_(self):
         return self.name
 
+class Stage(models.Model):
+    name = models.CharField(max_length=120, null=True, blank=True)
+    description = models.TextField(max_length=120, null=True, blank=True)
+    audience_cap = models.IntegerField(null=True, blank=True)
+    festival_id = models.ForeignKey(Festival, null=True, blank=True)
+    #schedule?
+
+
+    def __str__(self):
+        return self.name
+
 
 class Concert(models.Model):
-    name = models.CharField(max_length=120)
-    description = models.TextField(max_lenght=120)
+    name = models.CharField(max_length=120, null=True, blank=True)
+    description = models.TextField(max_length=120, null=True, blank=True)
     revenue = models.FloatField(null=True, blank=True)
-    stage_id = models.ForeignKey(Stage)
+    stage_id = models.ForeignKey(Stage, null=True, blank=True)
     sold_tickets = models.IntegerField(null=True, blank=True)
     audience_showed_up = models.IntegerField(null=True, blank=True)
-    concert_start_time = models.TimeDateField(null=True, blank=True)
-    concert_end_time = models.TimeDateField(null=True, blank=True)
-    tech_meetup_time = models.TimeDateField(null=True, blank=True)
-    tech_done_time = models.TimeDateField(null=True, blank=True)
+    concert_start_time = models.DateTimeField(null=True, blank=True)
+    concert_end_time = models.DateTimeField(null=True, blank=True)
+    tech_meetup_time = models.DateTimeField(null=True, blank=True)
+    tech_done_time = models.DateTimeField(null=True, blank=True)
     festival_id = models.ForeignKey(Festival, null=True, blank=True)
     # number_of_tech = models.IntegerField(null=True, blank=True)
 
@@ -48,4 +47,4 @@ class Concert(models.Model):
             super(Concert, self).save()
 
     def __str__(self):
-        return self.artist
+        return self.name
