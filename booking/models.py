02e4f0e3055d90d5b067f5db3e081263a2ac8992
Aslak Hollund <aslakhol@gmail.com>
2017-09-26 15:24:12 +0200

    sorted classes alphabetically

--- booking/models.py
+++ booking/models.py
@@ -1,26 +1,6 @@
 from django.db import models
 
 
-class Stage(models.Model):
-    name = models.CharField(max_length=120)
-    capacity = models.IntegerField()
-
-    def __str__(self):
-        return self.name
-
-
-class Festival(models.Model):
-    name = models.CharField(max_length=120)
-    start_date = models.DateField(null=True, blank=True)
-    end_date = models.DateField(null=True, blank=True)
-    start_time = models.TimeField(null=True, blank=True)
-    end_time = models.TimeField(null=True, blank=True)
-    tickets = models.IntegerField(null=True, blank=True)
-
-    def _str_(self):
-        return self.name
-
-
 class Concert(models.Model):
     artist = models.CharField(max_length=120)
     stage = models.ForeignKey(Stage)
@@ -41,3 +21,28 @@ class Concert(models.Model):
 
     def __str__(self):
         return self.artist
+
+
+class Festival(models.Model):
+    name = models.CharField(max_length=120)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    start_time = models.TimeField(null=True, blank=True)
+    end_time = models.TimeField(null=True, blank=True)
+    tickets = models.IntegerField(null=True, blank=True)
+
+    def _str_(self):
+        return self.name
+
+
+class Stage(models.Model):
+    name = models.CharField(max_length=120)
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
