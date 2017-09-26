37a0de0fad5a1e98a3e86cb7bcdaff2f852c1e8a
Ebba Fingarsen <ebbafin@gmail.com>
2017-09-26 17:23:02 +0200

    routed myconcerts, styled myconcerts

--- booking/urls.py
+++ booking/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from .views import (
     booking,
+    myconcerts,
 )
 
 
 urlpatterns = [
     url(r'^firstpage/$', booking, name='booking'),
+    url(r'^myconcerts/$', myconcerts, name='myconcerts'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -14,3 +14,14 @@ def booking(request):
     }
 
     return render(request, template_name, context)
+
+def myconcerts(request):
+    template_name = "booking/myconcerts.html"
+
+    myconcert_objs = Concert.objects.all()
+
+    context = {
+        'myconcerts': myconcert_objs,
+    }
+
+    return render(request, template_name, context)
