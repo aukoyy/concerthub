4048e4f1217373b4e6c9a1f9692c2dee6d952e3c
Sondre Wold <sondrewo@hotmail.com>
2017-10-19 13:02:26 +0200

    Linting

---
+++ static/js/map.js
@@ -0,0 +1,12 @@
+
+    function initMap() {
+        var uluru = {lat: 63.418110, lng: 10.405579};
+        var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: uluru
+        });
+        var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+        });
+    }
