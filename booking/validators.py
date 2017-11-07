f05ada8b6a024de85627076e287bbc955bfda03f
Aslak Hollund <aslakhol@gmail.com>
2017-11-07 16:18:16 +0100

    minor linting

--- booking/validators.py
+++ booking/validators.py
@@ -2,10 +2,11 @@ from django.core.exceptions import ValidationError
 from .login_tests import is_artist_manager
 from datetime import date
 
+
 def validate_even(value):
     if value % 2 != 0:
         raise ValidationError(
-            ('%(value)s is not an even number'),
+            '%(value)s is not an even number',
             params={'value': value},
         )
 
@@ -13,15 +14,14 @@ def validate_even(value):
 def validate_artist_manager(value, is_artist_manager):
     if not is_artist_manager:
         raise ValidationError(
-            ('%(value)s is not an artist manager'),
+            '%(value)s is not an artist manager',
             params={'value': value},
         )
 
 
 def validate_future(value):
     if value < date.today():
-        print('derp')
         raise ValidationError(
-            ('%(value)s is in the past'),
+            '%(value)s is in the past',
             params={'value': value},
         )
